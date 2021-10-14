d83a2c7ee888a580c95bd6d403d757423626e728
Andrew <kolbe1129@gmail.com>
2021-10-14 16:11:41 -0500

    added provide keyword to create dynamic titles

--- test/controllers/html_pages_controller_test.rb
+++ test/controllers/html_pages_controller_test.rb
@@ -4,15 +4,18 @@ class HtmlPageControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get html_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Reviews"
   end
 
   test "should get help" do
     get html_pages_help_url
     assert_response :success
+    assert_select "title", "Help | Reviews"
   end
 
   test "should get about" do
     get html_pages_about_url
     assert_response :success
+    assert_select "title", "About | Reviews"
   end
 end
